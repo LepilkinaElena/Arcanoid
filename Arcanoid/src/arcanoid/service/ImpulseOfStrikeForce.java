57f16e9d337ce0ab6b6a4431cbd704ba72216af1
LepilkinaElena <eklepilkina@gmail.com>
2014-10-06 20:23:54 +0400

    Add class of impulse of strike force

---
+++ Arcanoid/src/arcanoid/service/ImpulseOfStrikeForce.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.service;
+
+import arcanoid.model.FieldElement;
+
+/**
+ * Импульс силы удара
+ * 
+ * @author Елена
+ */
+public class ImpulseOfStrikeForce {
+    
+    /** Значение импульса */
+    private double value;
+    
+    /** 
+     * Конструктор
+     */
+    private ImpulseOfStrikeForce () {
+        value = 0;
+    }
+    
+    /**
+     * Рассчитать импульс силы удара элемента
+     * @param element 
+     */
+    public ImpulseOfStrikeForce count(FieldElement element) {
+        
+    }
+}
