a126da9dfaf41b313ac818f54134bded09fc32a1
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 22:37:11 +0400

    Add class for buffer

---
+++ Arcanoid/src/Buffer/Buffer.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buffer;
+
+import arcanoid.model.FieldElement;
+import com.golden.gamedev.object.Sprite;
+import java.util.HashMap;
+
+/**
+ * Буфер
+ * 
+ * @author Мария
+ */
+public class Buffer {
+    /** Таблица соответствий элемента поля и спрайта */
+    public HashMap<FieldElement, Sprite> table;
+    
+    /**
+     * Конструктор
+     */
+    public Buffer () {
+        table = new HashMap<FieldElement, Sprite>();
+    }
+    
+    /**
+     * Получить элемент поля
+     * @param sprate спрайт
+     * @return элемент поля
+     */
+    public FieldElement getElement(Sprite sprate) {
+        
+    }
+    
+    /**
+     * Получить спрайт
+     * @param element элемент
+     * @return спрайт
+     */
+    public Sprite getSprite(FieldElement element) {
+        
+    }
+    
+    /**
+     * Добавить пару
+     * @param element элемент
+     * @param sprate спрайт
+     */
+    public void addPair(FieldElement element, Sprite sprate) {
+        
+    }
+    
+    /**
+     * Удалить пару
+     * @param element элемент
+     */
+    public void deletePair(FieldElement element) {
+        
+    }
+}
