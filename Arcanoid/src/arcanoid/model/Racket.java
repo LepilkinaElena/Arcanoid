e18a97c0f957f29f0a45609af0f8ca4f37a9933e
LepilkinaElena <eklepilkina@gmail.com>
2014-10-07 18:58:57 +0400

    Add class for racket

---
+++ Arcanoid/src/arcanoid/model/Racket.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.model;
+
+/**
+ * Ракетка
+ * 
+ * @author Елена
+ */
+public class Racket extends FieldElement implements Bounced {
+    
+    @Override
+    public void handleCollision (ImpulseOfStrikeForce force) {
+        
+    }
+    
+    /**
+     * Толкнуть мяч
+     * 
+     * @param ball мяч
+     */
+    public void pushBall(Ball ball) {
+        
+    }
+}
