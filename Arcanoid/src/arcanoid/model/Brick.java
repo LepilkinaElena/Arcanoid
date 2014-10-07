4d8198ecc145fb40303c2ef739a2858b0fa2e531
LepilkinaElena <eklepilkina@gmail.com>
2014-10-07 18:57:55 +0400

    Add class for brick

---
+++ Arcanoid/src/arcanoid/model/Brick.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.model;
+
+import arcanoid.service.ImpulseOfStrikeForce;
+
+/**
+ * Кирпич
+ * 
+ * @author Елена
+ */
+public class Brick extends FieldElement implements Bounced {
+    /** Прочность*/
+    protected int strength;
+    
+    @Override
+    public void handleCollision (ImpulseOfStrikeForce force) {
+        
+    }
+}
