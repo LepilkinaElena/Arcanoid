63a7be0ed0fda1eb17bbb8411efb3c71b296b4d8
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 22:55:28 +0400

    Add class for view field element

---
+++ Arcanoid/src/View/ViewFieldElement.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import com.golden.gamedev.object.Sprite;
+
+/**
+ * Изображение элемента поля
+ * 
+ * @author Мария
+ */
+public abstract class ViewFieldElement {
+    /** Изображение */
+    private Sprite view;
+    /** Изображение при столкновении*/
+    private Sprite viewInCollision;
+    
+    /**
+     * Показать коллизию
+     */
+    public abstract void showCollision() ;
+}
