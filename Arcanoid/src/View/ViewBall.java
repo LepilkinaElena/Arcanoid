7cc2e32c80260261e1742996aca72a7b0806576d
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 22:57:06 +0400

    Add class for view ball

---
+++ Arcanoid/src/View/ViewBall.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import com.golden.gamedev.object.Sprite;
+
+/**
+ * Изображение мяча
+ * 
+ * @author Мария
+ */
+public class ViewBall extends ViewFieldElement{
+
+    /**
+     * Конструктор
+     * @param view изображение мяча
+     * @param viewInCollision изображение мяча при столкновении
+     */
+    public ViewBall(Sprite view, Sprite viewInCollision) {
+        
+    }
+    
+    /**
+     * Показать коллизию
+     */
+    @Override
+    public void showCollision() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
