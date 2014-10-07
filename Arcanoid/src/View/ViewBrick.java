0b06c2c3e7817d1af60de251dc9d25090fa72d1b
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 23:02:41 +0400

    Add class for view brick

---
+++ Arcanoid/src/View/ViewBrick.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import com.golden.gamedev.object.Sprite;
+
+/**
+ * Изображение кирпича
+ * 
+ * @author Мария
+ */
+public class ViewBrick  extends ViewFieldElement{
+
+    /**
+     * Конструктор
+     * @param view изображение кирпича
+     * @param viewInCollision изображение кирпича при столкновении
+     */
+    public ViewBrick(Sprite view, Sprite viewInCollision) {
+        
+    }
+    
+    /**
+     * Показать коллизию
+     */
+    @Override
+    public void showCollision() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
