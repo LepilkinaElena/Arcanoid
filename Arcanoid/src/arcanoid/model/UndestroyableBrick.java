cc84a1c7c30b2baa1c51703fec4478357d04f22a
LepilkinaElena <eklepilkina@gmail.com>
2014-10-07 18:58:41 +0400

    Add class for undestroyable brick

---
+++ Arcanoid/src/arcanoid/model/UndestroyableBrick.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.model;
+
+import arcanoid.service.ImpulseOfStrikeForce;
+
+/**
+ *
+ * @author Елена
+ */
+public class UndestroyableBrick extends Brick implements Bounced {
+    
+    @Override
+    public void handleCollision (ImpulseOfStrikeForce force) {
+        
+    }
+}
