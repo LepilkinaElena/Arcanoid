016a49c500b17c6364052baaa3914b498fe0b7d8
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 22:15:09 +0400

    Add class for game field

---
+++ Arcanoid/src/arcanoid/GameField.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcanoid;
+
+import arcanoid.model.FieldElement;
+
+/**
+ * Игровое поле
+ * 
+ * @author Мария
+ */
+public class GameField {
+    /**
+     * Конструктор
+     */
+    public GameField () {
+        
+    }
+    
+    /**
+     * Добавить элемент
+     * @param element элемент
+     */
+    public void addElement(FieldElement element) {
+        
+    }
+    
+    /**
+     * Удалить элемент
+     * @param element элемент
+     */
+    public void deleteElement(FieldElement element) {
+        
+    }
+    
+    /**
+     * Создать начальную обстановку
+     */
+    public void createInitialAmbiance() {
+        
+    }
+}
