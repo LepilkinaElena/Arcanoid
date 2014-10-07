9eb9008bf5379b65023d52694507fa7f136cf8d1
Kalinkina Maria <mashyfycya@gmail.com>
2014-10-07 21:56:20 +0400

    Add class for player

---
+++ Arcanoid/src/arcanoid/Player.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcanoid;
+
+import arcanoid.service.SpeedVector;
+
+/**
+ * Игрок
+ * 
+ * @author Мария
+ */
+public class Player {
+    /** Счет */
+    private int score;
+    /** Кол-во жизней */
+    private int lives;
+    
+    /**
+     *  Конструктор
+     */
+    public Player () {
+        score = 0;
+        lives = 0;
+    }
+    
+    /**
+     * Конструктор
+     * @param lives кол-во жизней
+     */
+    public Player (int lives) {
+        
+    }
+    
+    /**
+     * Задать количество жизней
+     * @param countLives кол-во жизней
+     */
+    public void setLives(int countLives) {
+        
+    }
+    
+    /**
+     * Получить кол-во жизней
+     * @return кол-во жизней
+     */
+    public int lives() {
+        
+    }
+    
+    /**
+     * Получить счет
+     * @return счет
+     */
+    public int score() {
+        
+    }
+    
+    /**
+     * Задать счет
+     * @param score счет 
+     */
+    public void setScore(int score) {
+        
+    }
+    
+    
+}
