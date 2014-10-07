aa0af706d7cd3ae7bed324d676a8136ba0c7ca1d
LepilkinaElena <eklepilkina@gmail.com>
2014-10-07 18:53:25 +0400

    Add interface for elements, from which other can bounce

---
+++ Arcanoid/src/arcanoid/model/Bounced.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.model;
+
+/**
+ * Элемент, от которого могут отскочить другие элементы
+ * 
+ * @author Елена
+ */
+public interface Bounced {
+    
+}
