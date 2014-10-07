5df2aebd79961c3d31371b173de7b0c06c1f5792
LepilkinaElena <eklepilkina@gmail.com>
2014-10-07 18:57:20 +0400

    Add class of collision handler for collision of unbouncing elements

---
+++ Arcanoid/src/arcanoid/model/CollisionHandlerUnbouncing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arcanoid.model;
+
+import java.util.ArrayList;
+
+/**
+ * Обработчик столкновений неотскакивающих элементов
+ * 
+ * @author Елена
+ */
+public class CollisionHandlerUnbouncing {
+    /** Упругий элемент*/
+    private Bouncing unbouncy;
+    /** Элементы, от которых упругий не отскочил*/
+    private ArrayList elements;
+    
+    /**
+     * Обработать столкновение
+     */
+    public void handleCollision() {
+        
+    }
+}
